d9810753b059e6f85d4d3503dbd0901039d09f04
Manav <manavmandli2990@gmail.com>
2024-09-28 02:25:16 +0530

    fix : changes

--- lms/lms/doctype/lms_quiz/lms_quiz.js
+++ lms/lms/doctype/lms_quiz/lms_quiz.js
@@ -2,8 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("LMS Quiz", {
-	// refresh: function(frm) {
-	// }
+	onload: function (frm) {
+		frm.get_field('questions').grid.cannot_add_rows = true;
+	},
+	add_multiple: function (frm) {
+    const dialog = new frappe.ui.form.MultiSelectDialog({
+        doctype: "LMS Question",
+        target: this.cur_frm,
+        columns: { 'question': null},
+        setters: {
+            'title': null,
+        },
+        action(selections) {
+            $.each(selections, function (index, item) {
+                var child = frm.add_child('questions');
+                child.question = item;
+            });
+            frm.refresh_field('questions');
+            dialog.dialog.hide();
+        },
+        onload_post_render: function () {
+            dialog.dialog.$wrapper.find(".btn-secondary").addClass("hidden");
+        },
+    });
+}
 });
 
 frappe.ui.form.on("LMS Quiz Question", {
